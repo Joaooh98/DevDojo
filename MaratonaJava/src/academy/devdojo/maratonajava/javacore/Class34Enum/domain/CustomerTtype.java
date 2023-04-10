c9a37b7267ec585b0c1db5da4b4c15436341faf0
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-04-10 09:04:31 -0300

    first commit

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Class34Enum/domain/CustomerTtype.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Class34Enum/domain/CustomerTtype.java
@@ -1,8 +1,45 @@
 package academy.devdojo.maratonajava.javacore.Class34Enum.domain;
 
 public enum CustomerTtype { // todos os atributos que nao criado nessa classe e constante
-    PESSOA_JURIDICA,
-    PESSOA_FISICA, 
-    PESSOA_EXTRANGEIRA,
-    PESSOA_SEM_TETO
+    PESSOA_JURIDICA(1,  "Pessoa fisica"),
+    PESSOA_FISICA (2, "Pessoa juridica");
+
+    private int VALOR;
+    private String NomeRelatorio;
+
+    
+
+    private CustomerTtype(int vALOR, String nomeRelatorio) {
+        VALOR = vALOR;
+        NomeRelatorio = nomeRelatorio;
+    }
+
+    /**
+     * @return int return the VALOR
+     */
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    /**
+     * @param VALOR the VALOR to set
+     */
+    public void setVALOR(int VALOR) {
+        this.VALOR = VALOR;
+    }
+
+    /**
+     * @return String return the NomeRelatorio
+     */
+    public String getNomeRelatorio() {
+        return NomeRelatorio;
+    }
+
+    /**
+     * @param NomeRelatorio the NomeRelatorio to set
+     */
+    public void setNomeRelatorio(String NomeRelatorio) {
+        this.NomeRelatorio = NomeRelatorio;
+    }
+
 }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 # DevDojo
 # DevDojo
 # DevDojo
+# DevDojo
