ec27c028b0e78a4c6b44b53c1b059c575e7b0460
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-06 23:17:18 -0300

    first commit

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Class46Nio/test/Teste01.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Class46Nio/test/Teste01.java
@@ -7,7 +7,7 @@ public class Teste01 {
     public static void main(String[] args) {
         Path p1 = Paths.get("/home/joao/Documentos/DevDojo/textClass46NIO.txt"); // so e permitido passar esse formato
       System.out.println(p1.getFileName());                                      // de "/" para o caminho pq o sistema
-                                                                                 // operacional e linux caso seja
-        }                                                                        // windows deveria utilizar dessa forma "//"
-                                                                                
+      System.out.println(p1.toFile());                                           // operacional e linux caso seja
+      System.out.println(p1.isAbsolute());                                       // windows deveria utilizar dessa forma "//"
+        }                                                                                                                                              
     }
