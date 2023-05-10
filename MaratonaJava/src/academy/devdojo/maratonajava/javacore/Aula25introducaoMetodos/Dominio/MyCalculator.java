661dece6cef7545ac2a0644114eaead32e2b7743
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-09 22:31:36 -0300

    first commit

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Dominio/MyCalculator.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Dominio/MyCalculator.java
@@ -2,4 +2,29 @@ package academy.devdojo.maratonajava.javacore.Aula25introducaoMetodos.Dominio;
 
 public class MyCalculator {
     
+    private double teclado;
+
+    private double resultado;
+
+    public void somar() {
+        
+    }
+
+    public double getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(double teclado) {
+        this.teclado = teclado;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    
 }
--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Test/MyCalculatorController.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Test/MyCalculatorController.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.Aula25introducaoMetodos.Dominio.MyC
 public class MyCalculatorController {
     public static void main(String[] args) {
         var MyCalculator = new MyCalculator();
-
+        
         
     }
 }
