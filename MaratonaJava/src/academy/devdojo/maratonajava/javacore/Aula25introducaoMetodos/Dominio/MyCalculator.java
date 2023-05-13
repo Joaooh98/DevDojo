b5fcf97d1a7df7e4a32b3620ddca917da3de9857
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-12 23:38:42 -0300

    first commit

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Dominio/MyCalculator.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Dominio/MyCalculator.java
@@ -1,19 +1,57 @@
 package academy.devdojo.maratonajava.javacore.Aula25introducaoMetodos.Dominio;
 
 public class MyCalculator {
-    
-    private Double resultado;
 
-    public Double somar(Double numberOne, Double numbertwo) {
-        resultado = numberOne + numbertwo;
-        return resultado;
+    private Double result;
+
+    public void add(Double numberOne, Double numberTwo) {
+        result = numberOne + numberTwo;
+        System.out.println("\n Soma dos 2 numeros: "+result);
     }
 
-    public double getResultado() {
-        return resultado;
+    public void toDecrease(Double numberOne, Double numberTwo) {
+        result = numberOne - numberTwo;
+        System.out.println("\n Subtração dos 2 numeros: "+result);
+    }
+
+    public void multiply(Double numberOne, Double numberTwo) {
+        result = numberOne * numberTwo;
+        System.out.println("\n Multiplicação dos 2 numeros: "+result);
+    }
+
+    public void division(Double numberOne, Double numberTwo) {
+        result = numberOne / numberTwo;
+        System.out.println("\n Divisão dos 2 numeros: "+result);
+    }
+
+    public void squareRoot(Double numberOne) {
+        result = Math.sqrt(numberOne);
+        System.out.println(result);
+    }
+
+    public void power(Double numberOne, Double numberTwo) {
+        result = Math.pow(numberOne, numberTwo);
+        System.out.println("\n Valor  dos "+numberOne+" elevado a potencia dos "+numberTwo+" = "+result);
+    }
+
+    public void pi(Double numberOne) {
+        result = (numberOne = Math.PI);
+        System.out.println(result);
+    }
+
+    public void percentage(Double numberOne, Double numberTwo){
+        var D = numberOne / 100;
+        result = D * numberTwo;
+
+        System.out.println("\n Valor em porcentagem dos "+numberOne+" do numero "+numberTwo+" = "+result);
+    }
+
+    public double getResult() {
+        return result;
     }
 
     public void setResultado(double resultado) {
-        this.resultado = resultado;
+        this.result = resultado;
     }
+
 }
--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Test/MyCalculatorController.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Aula25introducaoMetodos/Test/MyCalculatorController.java
@@ -8,12 +8,27 @@ public class MyCalculatorController {
     public static void main(String[] args) {
         var calc = new MyCalculator();
         try (Scanner t = new Scanner(System.in)) {
-            System.out.println("digite um numero:");
-            double num1 = t.nextDouble();
-            System.out.println("digite outro numero:");
-            double num2 = t.nextDouble();
-            calc.somar(num1, num2);
-            System.out.println(calc.getResultado());
+            System.out.println("você quer executar calculo raiz ou o PI de algum numero");
+            String d = t.next();
+            if (d.equalsIgnoreCase("sim")) {
+                System.out.println("digite um numero:");
+                double num = t.nextDouble();
+                System.out.println("valor de PI do numero informado:");
+                calc.pi(num);
+                System.out.println("valor da Raiz do numero informado:");
+                calc.squareRoot(num);
+                    } else {
+                            System.out.println("digite um numero:");
+                            double num1 = t.nextDouble();
+                            System.out.println("digite outro numero:");
+                            double num2 = t.nextDouble();
+                            calc.add(num1, num2);
+                            calc.toDecrease(num1, num2);
+                            calc.multiply(num1, num2);
+                            calc.power(num1, num2);
+                            calc.division(num1, num2);
+                            calc.percentage(num1, num2);
+            }
         }
     }
 }
