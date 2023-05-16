06a6bebbb88e3c8f192eee7c99d48f5b88ea79a4
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-15 22:36:40 -0300

    first commit

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Class46Nio/test/NormalizeTest03.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Class46Nio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest03 {
+    public static void main(String[] args) {
+        
+        String diritorioProjeto ="home/joao/dev";
+        String arquivoTxt = "../../arquivo.txt";
+
+        Path path01 = Paths.get(diritorioProjeto, arquivoTxt);
+        System.out.println(path01);
+        System.out.println(path01.normalize());
+        Path path02 = Paths.get("home/./joao/./dev");
+        System.out.println(path02);
+        System.out.println(path02.normalize());
+    }
+}
