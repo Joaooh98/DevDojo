c260b02fad52dd339ad7f4c9abd66c1f7df50862
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-31 22:42:34 -0300

    31-05-2023

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/ColecoesClass50/test/A07Comparator.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.ColecoesClass50.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import academy.devdojo.maratonajava.javacore.ColecoesClass50.domain.A01Sort;
+
+class MangasById implements Comparator<A01Sort> {
+
+    @Override
+    public int compare(A01Sort manga1, A01Sort manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+
+}
+
+public class A07Comparator {
+    public static void main(String[] args) {
+        List<A01Sort> mangas = new ArrayList<>(6);
+
+        mangas.add(new A01Sort(5L, "Hellsing Ultimate", 25));
+        mangas.add(new A01Sort(3L, "Attack on titan", 23));
+        mangas.add(new A01Sort(1L, "Berserk", 21));
+        mangas.add(new A01Sort(2L, "Pokemon", 22));
+        mangas.add(new A01Sort(4L, "Dragon Ball Z", 24));
+
+        mangas.stream()
+                .forEach(e -> System.out.println(e));
+
+        Collections.sort(mangas, new MangasById());
+        System.out.println("-----------------------------");
+
+        mangas.stream()
+                .forEach(e -> System.out.println(e));
+    }
+}
