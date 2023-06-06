5b4da1f69d01469ab1545bc4930fa4e5da8bb8c5
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-06-06 20:57:23 -0300

    06-06-2023, nao deu tempo de fazer o comite

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/ColecoesClass50/test/A12SeteHashSet.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.ColecoesClass50.test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import academy.devdojo.maratonajava.javacore.ColecoesClass50.domain.Manga;
+
+public class A12SeteHashSet {
+    public static void main(String[] args) {
+        List<Manga> mangas = new LinkedList<>();
+
+    }
+}
