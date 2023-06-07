9e3a733459b969b515370e945e1249eb901347bf
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-06-07 00:12:36 -0300

    complemento dia 06

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/ColecoesClass50/test/A12SeteHashSet.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/ColecoesClass50/test/A12SeteHashSet.java
@@ -9,5 +9,14 @@ public class A12SeteHashSet {
     public static void main(String[] args) {
         List<Manga> mangas = new LinkedList<>();
 
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 25, 0));
+        mangas.add(new Manga(3L, "Attack on titan", 23, 5));
+        mangas.add(new Manga(1L, "Berserk", 21, 4));
+        mangas.add(new Manga(2L, "Pokemon", 22, 2));
+        mangas.add(new Manga(4L, "Dragon Ball Z", 24, 1));
+
+        mangas.stream()
+                .forEach(e -> System.out.println(e));
+            
     }
 }
