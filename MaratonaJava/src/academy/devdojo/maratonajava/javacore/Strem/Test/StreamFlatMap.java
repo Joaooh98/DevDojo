5bc3747fa860bdf22ac186dee9a19031d895a0d9
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-08-01 23:11:18 -0300

    01-08-2023

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Strem/Test/StreamFlatMap.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Strem.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StreamFlatMap {
+    public static void main(String[] args) {
+        List<List<String>> devdojo = new ArrayList<>();
+
+        List<String> graphicDesigners = List.of("joao", "pedro", "camargo");
+        List<String> developers = List.of("Willian","David","Harisson");
+        List<String> students = List.of("mauricio","renan","fabio","wesley");
+
+        devdojo.add(students);
+        devdojo.add(developers);
+        devdojo.add(graphicDesigners);
+
+        for (List<String> people: devdojo) {
+            for (String person : people) {
+                System.out.println(person);
+            }
+        }
+
+        System.out.println("\n ------------- com stream -----------------\n");
+
+        devdojo.stream().flatMap(Collection::stream).forEach(System.out::println);
+    }
+}
