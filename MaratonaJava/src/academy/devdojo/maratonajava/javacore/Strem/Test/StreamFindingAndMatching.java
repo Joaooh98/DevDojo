b03172f097bf40e9bac38b1c6cc173a9386f9268
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-08-04 22:47:37 -0300

    04-08-2023

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Strem/Test/StreamFindingAndMatching.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Strem.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import academy.devdojo.maratonajava.javacore.Strem.domain.LightNovel;
+
+public class StreamFindingAndMatching {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Sword Art Online", 1.99),
+            new LightNovel("Sword Art Online", 1.99),
+            new LightNovel("Sword Art Online", 1.99),
+            new LightNovel("Re:Zero", 4.99),
+            new LightNovel("Attack on Titan", 2.99),
+            new LightNovel("Overlord ", 3.99),
+            new LightNovel("The Irregular at Magic High School", 6.99),
+            new LightNovel("No Game No Life", 9.99),
+            new LightNovel("Toradora! ", 7.99)));
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Strem/Test/StreamFindingAndMatching02.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Strem.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamFindingAndMatching02 {
+     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, 5, 7, 8, 9, 10);
+
+        // Verificando se há pelo menos um número par
+        
+        boolean hasEvenNumber = numbers.stream()
+                .anyMatch(number -> number % 2 == 0);
+
+        if (hasEvenNumber) {
+            System.out.println("A lista contém pelo menos um número par.");
+        } else {
+            System.out.println("A lista não contém números pares.");
+        }
+    }
+
+    //     public static void main(String[] args) {
+    //     List<Integer> numbers = Arrays.asList(1, 3, 5, 7, 8, 9, 10);
+
+    //     // Encontrando o primeiro número par 
+    
+    //     Optional<Integer> firstEvenNumber = numbers.stream()
+    //             .filter(number -> number % 2 == 0)
+    //             .findFirst();
+
+    //     if (firstEvenNumber.isPresent()) {
+    //         System.out.println("O primeiro número par encontrado é: " + firstEvenNumber.get());
+    //     } else {
+    //         System.out.println("Nenhum número par encontrado na lista.");
+    //     }
+    // }
+}
