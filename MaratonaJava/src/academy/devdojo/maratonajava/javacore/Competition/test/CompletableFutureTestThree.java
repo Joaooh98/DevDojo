8611dd393e008893a23f81c9c75e074cca27ab09
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-09-03 21:01:36 -0300

    03-09-2023

--- MaratonaJava/src/academy/devdojo/maratonajava/javacore/Competition/test/CompletableFutureTestThree.java
+++ MaratonaJava/src/academy/devdojo/maratonajava/javacore/Competition/test/CompletableFutureTestThree.java
@@ -4,44 +4,24 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-import academy.devdojo.maratonajava.javacore.Competition.service.StoreService;
-
+import academy.devdojo.maratonajava.javacore.Competition.service.StoreServiceDeprecated;
 
 public class CompletableFutureTestThree {
     public static void main(String[] args) {
-        var storeService = new StoreService();
-        searchPricesyncCompletableFuture(storeService);
-        System.out.println("----------------------------\n");
-        searchPriceAsyncCompletableFuture(storeService);
-        System.out.println("---------------------\n");
+        var storeServiceDeprecated = new StoreServiceDeprecated();
+        searchPricesyncCompletableFuture(storeServiceDeprecated);
+
     }
 
-    private static void searchPricesyncCompletableFuture(StoreService storeService) {
+    private static void searchPricesyncCompletableFuture(StoreServiceDeprecated storeService) {
         long start = System.currentTimeMillis();
 
         List<String> stores = List.of("Store One","Store two", "Store three","Store for");
 
-        List<Double> prices = stores.stream()
-                .map(storeService:: getPricesAsyncCompletableFuture)
-                .map(CompletableFuture::join)
-                .collect(Collectors.toList());
+        List<CompletableFuture<Double>> completableF = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getpriceSync(s))).collect(Collectors.toList());
 
-                System.out.println(prices);
-                System.out.println("joao");
-
-        long end = System.currentTimeMillis();
-        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
-    }
-
-    private static void searchPriceAsyncCompletableFuture(StoreService storeService) {
-        long start = System.currentTimeMillis();
-
-        List<String> stores = List.of("Store One","Store two", "Store three","Store for");
-
-        List<CompletableFuture<Double>> completableFutures = stores.stream().map(storeService:: getPricesAsyncCompletableFuture).collect(Collectors.toList());
-        List<Double> prices = completableFutures.stream().map(CompletableFuture::join).collect(Collectors.toList());
-
-                System.out.println(prices);
+                completableF.stream().map(CompletableFuture::join).collect(Collectors.toList());
 
         long end = System.currentTimeMillis();
         System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
